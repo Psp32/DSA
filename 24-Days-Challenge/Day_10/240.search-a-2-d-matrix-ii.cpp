e523ce153fdcafe274d62bb6036d0aefa9df8333
Psp32 <prempatro317@gmail.com>
2026-06-28 23:34:37 +0530

    searching in 2d matrix-2 done

---
+++ 24-Days-Challenge/Day_10/240.search-a-2-d-matrix-ii.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=240 lang=cpp
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            int n = matrix[i].size();
+            int low = 0;
+            int high = n-1;
+            if(matrix[i][low]<=target && target<=matrix[i][high]){
+                while(low<=high){
+                    int mid = (low+high)/2;
+                    if(target<matrix[i][mid]){
+                        high = mid-1;
+                    }
+                    else if(target>matrix[i][mid]){
+                        low = mid+1;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
