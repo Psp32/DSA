ceebba0f839d58ca2e7216ab7f5f4960ce637729
Psp32 <prempatro317@gmail.com>
2026-07-02 23:41:05 +0530

    Day 14 done

---
+++ 24-Days-Challenge/Day_14/206.reverse-linked-list.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ///try again this one later
+        if(head == NULL || head->next == nullptr){
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head ->next;
+        front -> next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
+// @lc code=end
+
